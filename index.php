af0e367c49c40ee96040f062b10fa025a6faea32
Jojie Avergonzado <jacdgroup@gmail.com>
2022-05-09 16:44:51 +0800

    update product list

---
+++ index.php
@@ -0,0 +1,315 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css" integrity="sha384-r4NyP46KrjDleawBgD5tp8Y7UzmLA05oM1iAEQ17CSuDqnUK2+k9luXQOfXJCJ4I" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap" rel="stylesheet">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title>Jhojie #dc3545 #15294c</title>
+    <style>
+  body, html {
+  height: 100%;
+  font-family: 'Poppins', sans-serif; 
+  
+} 
+@media only screen and (max-width: 986px) {
+    .buds {
+  /* The image used */
+  background-image: url("image/jije.png");
+
+  /* Full height */
+  height: 100%;
+
+  /* Center and scale the image nicely */
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+}
+	@media screen and (min-width: 500px) {
+    .buds { 
+	/* The image used */
+	background-image: url("image/jhojieRED1.jpg");
+
+	/* Full height */
+	height: 100%;
+
+	/* Center and scale the image nicely */
+	background-position: center;
+	background-repeat: no-repeat;
+	background-size: cover;
+		}
+    }
+	
+	@media screen and (max-width: 990px) and (min-width: 576px) {
+		.img-config{
+			width: 120% !important;
+			height: 40% !important;
+		}
+	}
+		@media screen and (max-width: 575px) {
+		.set-for-desc{
+			padding-left: 0% !important;
+			padding-right: 0% !important;
+		}
+	}
+
+
+.text-color{
+    color: #eb2c3c !important;
+    font-weight: bold;
+}
+
+          /* Stackoverflow preview fix, please ignore */
+    .navbar-nav {
+      flex-direction: row;
+    }
+    
+    .nav-link {
+      padding-right: .5rem !important;
+      padding-left: .5rem !important;
+    }
+    
+    /* Fixes dropdown menus placed on the right side */
+    .ml-auto .dropdown-menu {
+      left: auto !important;
+      right: 0px;
+    } 
+    .text-color-white{
+        color: rgb(255, 255, 255) !important;
+    }
+ .text-font{       
+    font-family: 'Poppins', sans-serif;
+}
+.addinghere{
+   padding-top: 10%;
+}
+.img-config{
+  width: 100%;
+  height: 100%;
+}
+.set-for-desc{
+  padding-left: 20%;
+  padding-right: 10%;
+}
+.email-tel{
+  text-decoration: none;
+}
+/* .btn-primary
+  {
+    color: #fff;
+    background-color: #1D3557 !important;
+    border-color: #1D3557 !important;
+    } */
+	
+	.btn-colors{
+	
+	color: #fff;
+    background-color: #15294c !important;
+    border-color: #15294c !important;
+	}
+	
+	.top-adjustment
+	{
+		padding-top: 10px;
+		padding-bottom: 20px;
+		
+	}
+	.pdng-here{
+		padding-bottom:5%;
+	}
+	
+	.lt-bg-col-foot
+	{
+		background-color: #eb2c3c !important;
+		padding: 20px 20px 20px 20px;
+		font-size: 40px;
+		color: #ffffff;
+	}
+	.dot-mail{
+		padding-bottom: 10px;
+		font-size: 30px;
+		color: #444b54;
+	}
+	.dot-mob{
+		padding-bottom: 10px;
+		font-size: 50px;
+		color: #444b54;
+		
+	}
+	.bg-footer{
+		background-color: #101e41;
+	}
+	
+	.testing {
+		padding-top: 40px;
+		padding-left: 10%;
+		color: #fff;
+	}
+	.border-baba{
+		    border: 1px solid rgba(255 255 255);
+	}
+    </style>
+  </head>
+  <body>   
+        <!--nav class="navbar navbar-expand-lg navbar-light fixed-top container">
+            <div class="container-fluid">
+              <a class="navbar-brand text-color" href="#">Jhojie</a>
+              <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                      <span class="navbar-toggler-icon"></span>
+                  </button>
+              <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <!-- ml-auto still works just fine->
+                <div class="navbar-nav ml-auto">
+                  <a class="nav-link active text-color" aria-current="page" href="#">HOME</a>
+                  <a class="nav-link text-color-white" href="#">ABOUT</a>
+                  <a class="nav-link text-color-white" href="#">SERVICES</a> 
+                  <a class="nav-link text-color-white" href="#">CONTACT</a> 
+                </div>
+              </div>
+            </div>
+          </nav> -->
+     
+        <!-- Columns are always 50% wide, on mobile and desktop -->
+        <div class="buds">
+            <div class="container">  
+                <div class="position-absolute top-50 translate-middle-y">
+                    <p class="text-start text-color">HI THERE,</p>
+                    <h1 class="text-color-white">IM JHOJIE</h1>
+                    <p class="text-start text-wrap text-color-white">
+					First and foremost, I'm grateful that you are here viewing my simple web portfolio design.<br/> 
+					This is made from scratch and challenges myself not to use a pre-made landing page <br/>
+					although this design is not unique at first sight, but I'm so proud of this self-made project.
+					</p> 
+					<a href="#next" class="btn btn-primary btn-color " tabindex="-1" role="button" aria-disabled="true">Continue Explore</a>
+
+                  </div> 
+            </div>
+        </div>
+        <div class="container">
+          <div class="row addinghere" id="next">
+		  <h1>About</h1>
+            <div class="col-sm adjuscent">
+
+              <img src="./image/darna.jpg" class="rounded mx-auto d-block img-config" alt="Darna">
+
+            </div>
+            <div class="col-sm">
+            <div class="set-for-desc">
+              <h4>I Am Web <text class="text-color">Developer</text></h4>
+              <p>
+                In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document
+                 or a typeface without relying on meaningful content.
+              <ul class="list-group list-group-flush">
+                <li class="list-group-item">Name: Jojie Avergonzado</li>
+                <li class="list-group-item">Age: 24</li>
+                <li class="list-group-item">Address: <Address>Lubogan Toril, Davao City, Philippines 8000</Address></li>
+             </ul>
+              <a href="https://docs.google.com/document/d/16cLHcvLfGC2cp-3OSIjZpaS5XajyNef5/edit?usp=sharing&ouid=114608249661662030800&rtpof=true&sd=true" class="btn btn-primary btn-sm" target="_blank" tabindex="-1" role="button" aria-disabled="true" >Open CV Here</a>
+            </div>
+            </div>
+          </div> 
+        </div> 
+<div class="container pdng-here"> 
+  <div class="row addinghere">
+	<h1>Product</h1>
+    <div class="col-sm text-center">
+	<img src="image/lumpyum.png" class="img-thumbnail" alt="...">
+    <div class="top-adjustment">
+	<h2 <text class="text-color">MEHDESSERT</text></h2>
+	<p>MehDessert is an entrepreneurial website that was made during my college days. The name right now was changed to Lumpyum after a month. This was a project by our team and used for selling dessert food. </p>
+		  <a href="https://lumpyum.herokuapp.com/" class="btn btn-primary btn-sm" target="_blank" tabindex="-1" role="button" aria-disabled="true" >Visit</a>
+	</div>
+	</div>
+    <div class="col-sm text-center">
+	<img src="image/medessert.png" class="img-thumbnail" alt="...">
+    <div class="top-adjustment">
+	<h2><text class="text-color">MEHDESSERTv2</text></h2>
+	<p>MehDessertv2 is a version 2 of the project mehdessert and made from a laravel framework this was made after of I was decided to upgrade the old website because PHP code getting depreciated.</p>
+		  <a href="https://mehdessertv2.herokuapp.com/" class="btn btn-primary btn-sm" target="_blank" tabindex="-1" role="button" aria-disabled="true" >Visit</a>
+	</div>
+	</div>
+    <div class="col-sm text-center">
+	<img src="image/reactis.png" class="img-thumbnail" alt="...">
+    <div class="top-adjustment">
+	<h2><text class="text-color">To Do App</text></h2>
+	<p>MehDessertv2 is a version 2 of the project mehdessert and made from a laravel framework this was made after I learned from my job and I was decided to apply this to my old website because PHP code getting depreciated.</p>
+		  <a href="https://reactpraktis.herokuapp.com/" class="btn btn-primary btn-sm" target="_blank" tabindex="-1" role="button" aria-disabled="true" >Visit</a>
+	</div>
+	</div>
+  </div>
+</div>
+<div class="bg-footer">
+  <!-- Stack the columns on mobile by making one full-width and the other half-width -->
+  <div class="row">
+    <div class="col-md-8 testing">  
+	 <div class="row">
+    <div class="col-sm">
+		<ul class="list-unstyled"> 
+			<li>It appears completely unstyled.</li>
+			<li>It appears completely unstyled.</li>
+			<li>It appears completely unstyled.</li>   
+		</ul>
+	</div>
+    <div class="col-sm">
+	<ul class="list-unstyled"> 
+			<li>It appears completely unstyled.</li>
+			<li>It appears completely unstyled.</li>
+			<li>It appears completely unstyled.</li>  
+		</ul>
+	</div>
+    <div class="col-sm">
+	<ul class="list-unstyled"> 
+			<li>It appears completely unstyled.</li>
+			<li>It appears completely unstyled.</li>
+			<li>It appears completely unstyled.</li>   
+		</ul>
+	</div>
+	</div> 
+		<hr />
+		<div class="row">
+			<div class="col-md-8"> 
+				2019 Copyright
+			</div>
+			<div class="col-md-4"> 
+				Terms and conditions
+			</div>
+		</div>
+	</div>
+	
+    <div class="col-6 col-md-4"> 
+	<div class="list-group"> 
+			<div class="d-flex justify-content-between align-items-center lt-bg-col-foot">
+
+			<span class=""><i class="fa fa-phone" aria-hidden="true"></i></span>
+			<span class="text-content">Let's Talk</span>
+                      
+	</div>
+  <div class="list-group-item list-group-item-action">
+    <div class="d-flex w-100 justify-content-between align-items-center">
+	<span class="dot-mob"><i class="fa fa-mobile" aria-hidden="true"></i></span>
+      <h5 class="mb-1"><a href="tel:+639103373780" class="email-tel">+639103373780</a></h5>
+    </div> 
+  </div>
+  <div class="list-group-item list-group-item-action">
+    <div class="d-flex w-100 justify-content-between align-items-center">
+	<span class="dot-mail"><i class="fa fa-paper-plane-o" aria-hidden="true"></i></span>
+      <h5 class="mb-1"><a href="mailto:jhojie.a@gmail.com" class="email-tel">jhojie.a@gmail.com</a></h5>
+    </div> 
+  </div>
+</div>
+	</div>
+  </div>
+    </div>
+ 
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+ 
+  </body>
+</html>
